84673e7205ca84f6b74dabdd263e68cb72f1faf5
Otabek Rakhimov <rahimovmail@gmail.com>
2023-01-16 16:50:48 -0800

    Add some contents to the project

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,11 +1,17 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
-  )
-}
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed obcaecati
+        omnis dolores ullam deserunt architecto sapiente aperiam, similique quo
+        laborum iusto velit alias aspernatur adipisci quos molestiae laudantium
+        eius minima et dignissimos ab eos nostrum? A dolore ipsam eum inventore.
+      </p>
+    </section>
+  );
+};
 
-export default About
+export default About;
